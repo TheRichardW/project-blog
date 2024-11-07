f3e9219f2e2a726d4e774e9f645b2c536e519f02
Richard Winkelaar <richard@adcalls.nl>
2024-11-07 14:41:29 +0100

    Exercise 3

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -7,8 +7,17 @@ import { loadBlogPost } from "@/helpers/file-helpers";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import Spinner from "@/components/Spinner";
 
+export async function generateMetadata({ params }) {
+  const blogPost = await loadBlogPost(params.postSlug);
+
+  return {
+    title: blogPost.frontmatter.title,
+  };
+}
+
 async function BlogPost({ params }) {
   const blogPost = await loadBlogPost(params.postSlug);
+  
   return (
     <article className={styles.wrapper}>
       <BlogHero title={blogPost.frontmatter.title} publishedOn={blogPost.frontmatter.publishedOn} />
--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,17 @@ import BlogSummaryCard from "@/components/BlogSummaryCard";
 
 import styles from "./homepage.module.css";
 import { getBlogPostList } from "@/helpers/file-helpers";
+import { BLOG_DESCRIPTION, BLOG_TITLE } from "@/constants";
+
+export const metadata = {
+  title: BLOG_TITLE,
+  description: BLOG_DESCRIPTION,
+};
 
 async function Home() {
   // TODO GET BLOGS OWN WAY
   const blogs = await getBlogPostList();
-  
+
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.mainHeading}>Latest Content:</h1>
